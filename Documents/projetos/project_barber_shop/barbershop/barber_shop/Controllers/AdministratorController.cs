d32736f4da8709be5a077610c8bdc0d68bea7f52
Leonardo Silva <leonardoferreira032@gmail.com>
2023-10-11 21:24:52 -0300

    Organizando página home do administrator

--- Documents/projetos/project_barber_shop/barbershop/barber_shop/Controllers/AdministratorController.cs
+++ Documents/projetos/project_barber_shop/barbershop/barber_shop/Controllers/AdministratorController.cs
@@ -23,39 +23,20 @@ namespace barber_shop.Controllers
             _barberShopRepository = barberShopRepository;
         }
 
-        public async Task<IActionResult> Index(User? user)
+        public async Task<IActionResult> Index(string? cpf)
         {
-            return View(user);
-        }
-
-        public async Task<IActionResult> Register()
-        {
-            //padronizando registro de um novo usuário
-            //quando o adm for registrar um usuário será redirecionado para a mesma página de cadastro que client
-            //mas com permissões diferentes (podendo escolher o perfil do usuário a ser cadastrado)
-            return RedirectToAction("Register", "Access");
-        }
-
-        public async Task<IActionResult> ManageAccount()
-        {
-            var users = await _barberShopRepository.GetAllUsers();
-            return View(users);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ManageAccount(string? cpf)
-        {
-            User user;
-            cpf = cpf.RemoveFormatCpf();
+            DashboardAdministratorViewModel viewModel = new DashboardAdministratorViewModel();
             if (cpf is not null)
             {
-                user = await _barberShopRepository.GetUserByCpf(cpf);
-                return RedirectToAction("Index", "Administrator", user);
+                var user = await _barberShopRepository.GetUserByCpf(cpf);
+                viewModel.User = user;
             }
-            user = await _barberShopRepository.GetUserByCpf(User.Identity.Name);
-            //retornar a view do cliente com os dados dele
-            return View();
+            else
+            {
+                var users = await _barberShopRepository.GetAllUsers();
+                viewModel.Users = users;
+            }
+            return View(viewModel);
         }
     }
 }
---
+++ Documents/projetos/project_barber_shop/barbershop/barber_shop/Models/ViewModel/DashboardAdministratorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace barber_shop.Models.ViewModel
+{
+    public class DashboardAdministratorViewModel
+    {
+        public User[]? Users { get; set; }
+        public User User { get; set; }
+    }
+}
