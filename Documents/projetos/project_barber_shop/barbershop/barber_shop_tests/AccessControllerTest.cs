fd2afea62a8469da861ff25a5e3bf61b1fb17e83
Leonardo Silva <leonardoferreira032@gmail.com>
2023-11-22 22:10:53 -0300

    Criando cenários de teste do página de login

---
+++ Documents/projetos/project_barber_shop/barbershop/barber_shop_tests/AccessControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Runtime;
+
+namespace barber_shop_tests
+{
+    [TestClass]
+    public class AccessControllerTest
+    {
+        [TestMethod]
+        [Owner("Leonardo")]
+        [Description("Verifica se usuário está tendo acesso com dados válidos.")]
+        public void LoginWithValidUser()
+        {
+        }
+
+        [TestMethod]
+        [Owner("Leonardo")]
+        [Description("Verificar se usuário obtem erro ao logar com os dados inválidos.")]
+        public void LoginWithInvalidUser()
+        {
+        }
+    }
+}
